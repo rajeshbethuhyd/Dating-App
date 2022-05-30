b9405bc3cd6a80c3573a60279064e52c55edbe2f
Rajesh Bethu <rajeshbethu25@gmail.com>
2022-05-30 22:56:26 +0530

    setLocation DONE

--- src/screens/temp.js
+++ src/screens/temp.js
@@ -1,60 +1,20 @@
 import {View, Text} from 'react-native';
-import GetLocation from 'react-native-get-location';
 import React, {useState, useEffect} from 'react';
 import database from '@react-native-firebase/database';
-import RNLocation from 'react-native-location';
-// RNLocation.configure({
-//   distanceFilter: 5.0,
-// });
+import {GeoFire} from 'geofire';
 
 export default function Temp() {
-  const [location, setLocation] = useState(null);
-  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    permissionHandle();
+    getLocations();
   }, []);
-  const permissionHandle = async () => {
-    console.log('line 13');
-    let permission = await RNLocation.checkPermission({
-      android: {
-        detail: 'fine', // or 'coarse'
-      },
-    });
-    let temp_location;
-    if (!permission) {
-      permission = await RNLocation.requestPermission({
-        ios: 'whenInUse',
-        android: {
-          detail: 'fine',
-          rationale: {
-            title: 'We need to access your location',
-            message: 'We use your location to show where you are on the map',
-            buttonPositive: 'OK',
-            buttonNegative: 'Cancel',
-          },
-        },
-      });
-      temp_location = await RNLocation.getLatestLocation({timeout: 100});
-      setLocation(temp_location);
-      setLoading(false);
-    } else {
-      temp_location = await RNLocation.getLatestLocation({timeout: 100});
-      setLocation(temp_location);
-      setLoading(false);
-    }
+
+  const getLocations = () => {
+    const geoFireRef = new GeoFire(database().ref('/geoData/'));
+    geoFireRef.set('test', [12.492379, 78.516338]);
   };
-  if (location === null) {
-    console.log('line 46');
-    return (
-      <View>
-        <Text>Loading</Text>
-      </View>
-    );
-  } else {
-    return (
-      <View>
-        <Text>{location.latitude + ', ' + location.longitude}</Text>
-      </View>
-    );
-  }
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  );
 }
